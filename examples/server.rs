c2f9adfe96406323e715513444a9d70fc7c4c17d
Teddy Wing <git@teddywing.com>
2020-06-28 23:48:54 +0200

    examples/server: Try using a `conduit_router::RouteBuilder`
    Try making a server with more than one route.

--- examples/server.rs
+++ examples/server.rs
@@ -1,4 +1,5 @@
 extern crate conduit;
+extern crate conduit_router;
 extern crate http;
 extern crate fastcgi_conduit;
 
@@ -6,14 +7,20 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io;
 
-use conduit::{Body, RequestExt, Response};
+use conduit::{Body, HttpResult, RequestExt, Response};
 use conduit::header;
+use conduit_router::{RequestParams, RouteBuilder};
 
 use fastcgi_conduit::Server;
 
 
 fn main() {
-    Server::start(handler);
+    let mut router = RouteBuilder::new();
+
+    router.get("/", handler);
+    router.get("/:var", var_handler);
+
+    Server::start(router);
 }
 
 fn handler(req: &mut dyn RequestExt) -> io::Result<Response<Body>> {
@@ -24,4 +31,11 @@ fn handler(req: &mut dyn RequestExt) -> io::Result<Response<Body>> {
             .body(Body::from_static(b"<h1>Test</h1>"))
             .unwrap()
     )
+}
+
+fn var_handler(req: &mut dyn RequestExt) -> HttpResult {
+    let var = req.params().find("var").unwrap();
+    let text = format!("The value is: {}", var).into_bytes();
+
+    Response::builder().body(Body::from_vec(text))
 }
